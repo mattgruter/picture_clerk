8c0602306cf61559bc5fab2d959a8706fb5f71e7
Matthias Grüter <matthias@grueter.name>
2011-09-25 02:45:08 +0200

    Clean-up in connector tests

--- picture_clerk/test/test_connector.py
+++ picture_clerk/test/test_connector.py
@@ -5,20 +5,19 @@ from connector import Connector
 
 class ConnectorObjectCreationTest(unittest.TestCase):
     url = "testurl"
-    
-    def setUp(self):
-        self.connector = Connector(self.url) 
-    
+
     def testAttributes(self):
         """
         __init__ should store supplied attributes and initialize all properties
         """
+        self.connector = Connector(self.url) 
         self.assertEqual(self.url, self.connector.url)
 
     def testNotConnected(self):
         """
         __init__ should set isconnected to False upon object creation
         """
+        self.connector = Connector(self.url) 
         self.assertFalse(self.connector.isconnected)
         
         
