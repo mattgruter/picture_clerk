cb8203d2197bc7b634ea89d99fb20f0153b81127
Matthias Grüter <matthias@grueter.name>
2012-02-12 13:32:21 +0100

    Removed unused config attribute in Repo class.

--- picture_clerk/repo.py
+++ picture_clerk/repo.py
@@ -17,7 +17,7 @@ class PictureAlreadyIndexedError(Exception):
     def __str__(self):
         return "%s already in index" % self.pic
     def __repr__(self):
-        return "PictureAlreadyIndexedError(%s)" % self.pic 
+        return "PictureAlreadyIndexedError(%s)" % self.pic
 
 class PictureNotIndexedError(KeyError):
     def __init__(self, pic):
@@ -26,7 +26,7 @@ class PictureNotIndexedError(KeyError):
     def __str__(self):
         return "%s not in index" % self.pic
     def __repr__(self):
-        return "PictureNotIndexedError(%s)" % self.pic 
+        return "PictureNotIndexedError(%s)" % self.pic
 
 
 class Repo(object):
@@ -35,47 +35,46 @@ class Repo(object):
     """
     def __init__(self, index=dict()):
         self.index = index
-        self.config = None
-        
+
     def __repr__(self):
-        return "Repo(%s, %s)" % (self.config, self.index)
-    
+        return "Repo(%s)" % self.index
+
     def __str__(self):
         return repr(self)
-    
+
     def __eq__(self, other):
         return self.index == other.index
-        
+
     def __ne__(self, other):
         return not self.__eq__(other)
-    
+
     def add_picture(self, pic):
         key = pic.filename
         if key in self.index:
             raise PictureAlreadyIndexedError(pic.filename)
         log.info("Adding %s.", pic.filename)
         self.index[key] = pic
-        
+
     def add_pictures(self, pics):
         for pic in pics:
             try:
                 self.add_picture(pic)
             except PictureAlreadyIndexedError as paie:
                 log.info("%s already in index", paie.pic)
-        
+
     def get_pictures_iter(self):
         return self.index.itervalues()
-        
+
     def get_pictures(self):
         return list(self.get_pictures_iter())
-        
+
     def get_picture_by_filename(self, filename):
         log.info("Fetching %s from repository.", filename)
         try:
             return self.index[filename]
         except KeyError:
             raise PictureNotIndexedError(filename)
-    
+
     def update_picture(self, pic):
         key = pic.filename
         if key not in self.index:
