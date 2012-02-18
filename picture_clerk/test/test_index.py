0518db600b57c9a55a53f209510126e3c2e7d0c9
Matthias Grüter <matthias@grueter.name>
2012-02-18 14:31:09 +0100

    Added some tests for PictureIndex's read/write methods.

--- picture_clerk/test/test_index.py
+++ picture_clerk/test/test_index.py
@@ -4,97 +4,111 @@ Created on 2011/03/08
 @author: Matthias Grueter <matthias@grueter.name>
 @copyright: Copyright (c) 2011 Matthias Grueter
 @license: GPL
+
 """
 import unittest
 import mock
+import StringIO
 
 from index import PictureIndex
-from index import PictureAlreadyIndexedError, PictureNotIndexedError
+from index import PictureAlreadyIndexedError, PictureNotIndexedError, IndexParsingError
 
 
-class ObjectCreationTest(unittest.TestCase):
-    def setUp(self):
-        self.mock_index = mock.Mock()
+class BasicTests(unittest.TestCase):
 
-    def test_attributes(self):
-        """Constructor should return a new Index with supplied contents."""
-        pi = PictureIndex(index=self.mock_index)
-        self.assertIsInstance(pi, PictureIndex)
-        self.assertIs(pi.index, self.mock_index)
-
-
-class EqualityTest(unittest.TestCase):
-    def setUp(self):
-        self.index = ['pic1', 'pic2', 'pic3']
-        self.instance = PictureIndex(self.index)
-
-    def test_equal(self):
-        other = PictureIndex(self.index)
-        self.assertTrue(self.instance == other)
-        self.assertFalse(self.instance != other)
-
-    def test_notequal(self):
-        other = PictureIndex([])
-        self.assertTrue(self.instance != other)
-        self.assertFalse(self.instance == other)
-
-
-class IndexTest(unittest.TestCase):
     def setUp(self):
         self.mock_pic = mock.Mock()
-        self.index = dict()
-        self.pi = PictureIndex(index=self.index)
+        self.index = {'pic1': 'file1', 'pic2': 'file2', 'pic3': 'file3'}
+
+    def test_attributes(self):
+        mock_index = mock.Mock()
+        pi = PictureIndex(index=mock_index)
+        self.assertIsInstance(pi, PictureIndex)
+        self.assertIs(pi.index, mock_index)
+
+    def test_equalality(self):
+        index = {'pic1': 'file1', 'pic2': 'file2', 'pic3': 'file3'}
+        self.assertEqual(PictureIndex(index), PictureIndex(index))
+        self.assertNotEqual(PictureIndex(index), PictureIndex())
+        self.assertNotEqual(PictureIndex(index),
+                            PictureIndex({'pic1': 'file1'}))
 
     def test_add_picture(self):
-        """Added picture instance should end up in index."""
-        self.pi.add_picture(self.mock_pic)
+        pi = PictureIndex(self.index)
+        pi.add_picture(self.mock_pic)
         self.assertIn(self.mock_pic, self.index.values())
 
-    def test_readd_picture(self):
-        """Adding a picture twice should raise PictureAlreadyIndexedError."""
-        self.pi.add_picture(self.mock_pic)
+    def test_readd_picture_error(self):
+        pi = PictureIndex(self.index)
+        pi.add_picture(self.mock_pic)
         self.assertRaises(PictureAlreadyIndexedError,
-                          self.pi.add_picture, self.mock_pic)
+                          pi.add_picture, self.mock_pic)
 
     def test_add_get_pictures(self):
-        """Added list of pictures should all end up in index."""
+        pi = PictureIndex(self.index)
         mock_pics = [mock.Mock(), mock.Mock(), mock.Mock()]
-        self.pi.add_pictures(mock_pics)
+        pi.add_pictures(mock_pics)
         for mock_pic in mock_pics:
-            self.assertIn(mock_pic, self.pi.get_pictures())
+            self.assertIn(mock_pic, pi.get_pictures())
 
     def test_get_pictures_iter(self):
-        """get_pictures_iter should return an iterator over all pics."""
+        pi = PictureIndex(self.index)
         mock_pics = [mock.Mock(), mock.Mock(), mock.Mock()]
-        self.pi.add_pictures(mock_pics)
-        self.assertSequenceEqual(self.pi.get_pictures(),
-                                 list(self.pi.get_pictures_iter()))
+        pi.add_pictures(mock_pics)
+        self.assertSequenceEqual(pi.get_pictures(),
+                                 list(pi.get_pictures_iter()))
 
     def test_get_picture_by_filename(self):
-        """Supplying picture filename should return indexed instance."""
-        self.pi.add_picture(self.mock_pic)
+        pi = PictureIndex(self.index)
+        pi.add_picture(self.mock_pic)
         key = self.mock_pic.filename
-        pic = self.pi.get_picture_by_filename(key)
+        pic = pi.get_picture_by_filename(key)
         self.assertEqual(pic, self.mock_pic)
 
     def test_get_picture_by_filename_error(self):
-        """Trying to fetch unknown pic should raise PictureNotIndexedError."""
+        pi = PictureIndex(self.index)
         self.assertRaises(PictureNotIndexedError,
-                          self.pi.get_picture_by_filename, "unknown")
+                          pi.get_picture_by_filename, "unknown")
 
     def test_update_picture(self):
-        """Updating indexed picture replace existing picture with new one."""
-        self.pi.add_picture(self.mock_pic)
+        pi = PictureIndex(self.index)
+        pi.add_picture(self.mock_pic)
         new_mock_pic = mock.Mock()
         new_mock_pic.filename = self.mock_pic.filename
-        self.pi.update_picture(new_mock_pic)
+        pi.update_picture(new_mock_pic)
         self.assertIn(new_mock_pic, self.index.values())
         self.assertNotIn(self.mock_pic, self.index.values())
 
     def test_update_picture_error(self):
-        """Trying to update unknown pic should raise PictureNotIndexedError."""
+        pi = PictureIndex(self.index)
         self.assertRaises(PictureNotIndexedError,
-                          self.pi.update_picture, self.mock_pic)
+                          pi.update_picture, self.mock_pic)
+
+
+class ReadWriteTest(unittest.TestCase):
+
+    def setUp(self):
+        self.fh = StringIO.StringIO() # file-like string buffer
+
+    def tearDown(self):
+        self.fh.close()
+
+    def test_write_read_cycle(self):
+        index_a = PictureIndex({'pic1': "Picture 1", 'pic2': "Picture 2"})
+        index_a.write(self.fh)
+        self.fh.seek(0) # rewind buffer
+        index_b = PictureIndex()
+        index_b.read(self.fh)
+        self.assertEqual(index_a, index_b, "Dumped and read index don't match.")
+        self.assertIsNot(index_a, index_b, "Dumped and read index is same obj.")
+
+    def test_read_error(self):
+        # creating buffer content that can't be unpickled
+        corrupt_buf = "42\n"
+        self.fh.write(corrupt_buf)
+        self.fh.seek(0) # rewind buffer
+        pi = PictureIndex()
+        self.assertRaises(IndexParsingError, pi.read, self.fh)
 
 
 if __name__ == "__main__":
