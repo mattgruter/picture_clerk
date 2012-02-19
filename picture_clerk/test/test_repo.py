e201b118b1bb20bf3866f8e52a756a912e13d9db
Matthias Grüter <matthias@grueter.name>
2012-02-19 01:17:32 +0100

    Unittest for repo factory method "load_from_disk".

--- picture_clerk/test/test_repo.py
+++ picture_clerk/test/test_repo.py
@@ -7,6 +7,13 @@ Created on 2011/08/09
 """
 import unittest
 import mock
+import os
+import StringIO
+import contextlib
+
+import connector
+import config
+import index
 
 from repo import Repo
 
@@ -22,12 +29,61 @@ class BasicTests(unittest.TestCase):
         pass
 
     def test_attributes(self):
-        """Constructor should return a new Repo with supplied contents."""
         repo = Repo(self.index, self.config, self.connector)
         self.assertIsInstance(repo, Repo)
         self.assertIs(repo.index, self.index)
         self.assertIs(repo.config, self.config)
         self.assertIs(repo.connector, self.connector)
+
+    @unittest.skip
+    def test_save_to_disk(self):
+        #@todo
+        self.assertTrue(False)
+
+
+class FactoryTests(unittest.TestCase):
+
+    def mock_open(self, path, mode):
+        if os.path.basename(path) == 'config':
+            return self.conf_buf
+        else:
+            return self.pi_buf
+
+    def create_mock_connector(self):
+        con = mock.Mock(spec_set=connector.Connector)
+        con.exists = mock.Mock(return_value=True)
+        con.open = self.mock_open
+        return con
+
+    def create_mock_file(self, obj):
+        buf = StringIO.StringIO()
+        obj.write(buf)
+        buf.seek(0)
+        cm = contextlib.closing(buf)
+        return cm
+
+    def setUp(self):
+        self.conf = config.Config.from_dict({'index.file': 'index-path'})
+        self.pi = index.PictureIndex({'pic1': 'fileA', 'pic2': 'fileB'})
+
+    @unittest.skip
+    def test_create_on_disk(self):
+        #@todo
+        self.assertTrue(False)
+
+    def test_load_from_disk(self):
+        self.conf_buf = self.create_mock_file(self.conf)
+        self.pi_buf = self.create_mock_file(self.pi)
+        con = self.create_mock_connector()
+        repo = Repo.load_from_disk(con)
+        self.assertEqual(repo.connector, con)
+        self.assertEqual(repo.config['index.file'], 'index-path')
+        self.assertEqual(repo.index, self.pi)
+
+    @unittest.skip
+    def test_clone(self):
+        #@todo
+        self.assertTrue(False)
 
 
 if __name__ == "__main__":
